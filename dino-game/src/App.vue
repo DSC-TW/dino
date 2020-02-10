c3d019792e693cfed08fed68beb349c9c7444f74
alanhc <alanhc.tseng1999@gmail.com>
2020-02-10 11:40:03 +0800

    rebase master

--- dino-game/src/App.vue
+++ dino-game/src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card :cardData="cardData" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import Card from './components/Card'
+  export default {
+    name: 'app',
+    components: {
+      Card
+    },
+    data() {
+      return {
+        cardData:[
+          {
+            name:'Adam',
+            birthday:'1985.08.12',
+            mail:'adam@xxx.com'
+          },
+          {
+            name:'Jack',
+            birthday:'1980.02.23',
+            mail:'jack@xxx.com'
+          },
+          {
+            name:'Candy',
+            birthday:'1982.05.06',
+            mail:'candy@xxx.com'
+          },
+          {
+            name:'Louis',
+            birthday:'1984.11.21',
+            mail:'loui@xxx.com'
+          },
+          {
+            name:'Lurry',
+            birthday:'1982.01.17',
+            mail:'lurry@xxx.com'
+          }
+        ]
+      }
+    } 
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ dino-game/src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="card_wall">
+        <div class="card" v-for="item in cardData" :key="item.name">
+            <div class="card_title">{{ item.name }}</div>
+            <div class="card_body">
+                <p>生日：{{ item.birthday }}</p>
+                <p>e-mail：{{ item.mail }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        cardData: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .card_wall {
+        display: inline-flex;
+        flex-wrap: wrap;
+    }
+    .card {
+        width: 300px;
+        margin: 10px;
+        background: white;
+        border: 1px solid #cccccc;
+        border-radius: 6px;
+        box-shadow: 0 0 4px 0 rgba(0,0,0,0.2);
+    }
+    .card_title {
+        padding: 16px;
+        border-bottom: 1px solid #cccccc;
+        color: black;
+        font-size: 20px;
+        line-height: 26px;
+        font-weight: bold;
+    }
+    .card_body {
+        padding: 16px;
+        color: #555555;
+        font-size: 16px;
+        line-height: 22px;
+    }
+    p {
+        margin: 0 0 10px 0;
+    }
+</style>
