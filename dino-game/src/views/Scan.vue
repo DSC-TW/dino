10710ff552daaca59cf5dab7a0a88e26b98649c2
littlechintw <littlechintw@gmail.com>
2020-02-17 15:04:45 +0800

    [Feature] Create QRcode Reader

--- dino-game/src/views/Scan.vue
+++ dino-game/src/views/Scan.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="scan">
     <h1>This is a scan page</h1>
+    <Scan/>
   </div>
 </template>
+<script>
+import Scan from '../components/Scan.vue'
+export default {
+  name: 'register',
+  components: {
+    Scan
+  }
+}
+</script>
