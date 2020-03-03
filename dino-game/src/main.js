5dac8950906f05bbc84b1be7bc8ee7d7af7fcc15
littlechintw <littlechintw@gmail.com>
2020-03-03 23:33:13 +0800

    [Feature] Create a user

--- dino-game/src/main.js
+++ dino-game/src/main.js
@@ -8,9 +8,13 @@ import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import VueResource from 'vue-resource';
 /* 等校 var VueResource = require('vue-resource'); */
+import VueQRCodeComponent from 'vue-qrcode-component'
+Vue.component('qr-code', VueQRCodeComponent)
 
+import VueCookie from 'vue-cookie'
+
+Vue.use(VueCookie)
 Vue.use(VueResource);
-
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
